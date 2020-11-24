8a34587ec5dfb22f8a2e5b95dbdc63c112ae3de3
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-11-24 04:45:39 +0200

    class32

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import useForm from './hooks/useForm';
 function FormHook(props) {
+  const [values, handleInputChange, handleSubmit] = useForm(handleSubmitToHook);
+  console.log(props);
   const [item, setItem] = useState({});
-  const handleInputChange = e => {
-    setItem({ ...item, [e.target.name]: e.target.value });
-  };
-  const handleSubmit = e => {
+  // const handleInputChange = e => {
+  //   setItem({ ...item, [e.target.name]: e.target.value });
+  // };
+  function handleSubmitToHook(e) {
     e.preventDefault();
+    console.log(e);
     e.target.reset();
-    props.handleSubmit(item);
+    props.handleSubmit(values);
     const emptyItem = {};
     setItem({ item: emptyItem });
-  };
+  }
 
   return (
     <>
-      <form onSubmit={handleSubmit} class="border p-3">
+      <form
+        onSubmit={e => {
+          handleSubmit(e);
+        }}
+        className="border p-3"
+      >
         <h3>Add Item</h3>
         <Form.Group controlId="newToDoItem">
           <Form.Label>To Do Item</Form.Label>
@@ -24,6 +33,7 @@ function FormHook(props) {
             placeholder="Add To Do List Item "
             name="text"
             onChange={handleInputChange}
+            required
           />
         </Form.Group>
         <Form.Group controlId="Difficulty Rating">
@@ -31,10 +41,12 @@ function FormHook(props) {
           <Form.Control
             type="range"
             placeholder="Add To Do List Item "
+            defaultValue="3"
             min="1"
             max="5"
             name="difficulty"
             onChange={handleInputChange}
+            required
           />
         </Form.Group>
         <Form.Group controlId="assignedTo">
@@ -42,11 +54,12 @@ function FormHook(props) {
           <Form.Control
             type="text"
             placeholder="Assigned To"
-            name="text"
+            name="assignee"
             onChange={handleInputChange}
+            required
           />
         </Form.Group>
-        <Button variant="primary" className="align-self-start">
+        <Button variant="primary" className="align-self-start" type="submit">
           Add Item
         </Button>
       </form>
